236a079ae740fac9d9e3a7dcab10479bc43fc8bd
nstbayless <nstbayless@gmail.com>
2022-04-30 13:25:07 -0700

    api rename

--- coroutine.h
+++ coroutine.h
@@ -10,40 +10,26 @@ extern "C" {
 #define PDCO_CPPONLY(x)
 #endif
 
-typedef void(*pdco_fn_t)(void);
+
+typedef int pdco_handle_t;
+typedef pdco_handle_t(*pdco_fn_t)(pdco_handle_t caller);
 
 // Please note that none of these functions are thread-safe.
 // (They are, of course, coroutine-safe.)
 
-// call from outside a coroutine
-// starts a new coroutine
-// returns id of coroutine, or 0 if already complete, or -1 if an error occurred.
-int pdco_run(
+pdco_handle_t pdco_current(void);
+pdco_handle_t pdco_create(
     pdco_fn_t fn,
-    size_t stacksize    PDCO_CPPONLY(=0)
+    size_t stacksize    PDCO_CPPONLY(=0),
+    void* ud            PDCO_CPPONLY(=NULL),
+    size_t udsize       PDCO_CPPONLY(=0),
 );
 
-// call from outside a coroutine.
-// returns 0 if completed
-// returns -1 if error (e.g. coroutine finished/no such coroutine)
-// returns id of the coroutine otherwise.
-int pdco_resume(int co); 
+// crashes if h is invalid or ended
+void pdco_resume(pdco_handle_t h);
 
-// call from outside a coroutine.
-// Ends a coroutine prematurely. Not normally needed. Not recommended.
-// returns 0 if deleted, -1 if an error occurred, and co if co not found.
-int pdco_kill(int co);
-
-// call from within a coroutine.
-void pdco_yield(void);
-
-// returns the id of the active coroutine, or 0 if none are active.
-int pdco_get_coroutine(void);
-
-// returns a pointer to a per-coroutine user-definable void* pointer.
-// if co is 0, returns the void* associated with the current coroutine.
-// returns null if no such coroutine.
-void** pdco_ud(int co);
+// gets user-defined local value
+void* pdco_ud(int co);
 
 #ifdef __cplusplus
 }
