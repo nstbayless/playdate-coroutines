f76220e5c65462be2c7254e0e5d3d4057aa3d1c2
nstbayless <nstbayless@gmail.com>
2022-05-01 18:27:58 -0700

    canary

--- coroutine.c
+++ coroutine.c
@@ -9,6 +9,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if !defined(TARGET_PLAYDATE) || defined(NEWLIB_COMPLETE)
+#include <assert.h>
+#include <stdio.h>
+#endif
+
 #include "coroutine.h"
 
 #if __has_include(<ucontext.h>)
@@ -20,6 +25,10 @@
 #endif
 
 #pragma GCC optimize("O1")
+
+#ifndef NDEBUG
+static const char* canary_context = "";
+#endif
 
 __attribute__((noinline))
 static int stack_direction_helper(int* x)
@@ -35,8 +44,14 @@ static inline int stack_direction(void)
     return stack_direction_helper(&x);
 }
 
+#define CANARYC1 0xC0DEDEAD
+#define CANARYC2 0xDEADC0DE
+#define CANARYS1 0xDEADDA7E
+#define CANARYS2 0xDA7EDEAD
+
 typedef struct coroutine_t
 {
+    int canary1;
     pdco_handle_t id;
     
     // next in linked list
@@ -58,22 +73,33 @@ typedef struct coroutine_t
     #else // USE_UCONTEXT
     ucontext_t ucalt;
     #endif
+    int canary2;
 } coroutine_t;
 
-static coroutine_t co_main = {
-    .id = PDCO_MAIN_ID,
-    .status = 1,
-    .ud = NULL
-};
+static coroutine_t co_main;
 
 // linked list
 static coroutine_t* pdco_first = NULL;
 
 // Current coroutine
 // It is the caller's responsibility to maintain this.
-static coroutine_t* volatile pdco_active = &co_main;
+static coroutine_t* volatile pdco_active = NULL;
 static coroutine_t* volatile pdco_prev = NULL;
 static int next_coroutine_id = PDCO_MAIN_ID + 1;
+
+static int is_init = 0;
+static void init(void)
+{
+    if (!is_init)
+    {
+        co_main.id = PDCO_MAIN_ID;
+        co_main.status = 1;
+        co_main.ud = NULL;
+        pdco_active = &co_main;
+        
+        is_init = 1;
+    }
+}
 
 // delete coroutine if it exists and is not main
 static void delco(int id)
@@ -106,10 +132,8 @@ static inline void cleanup_if_complete(coroutine_t* co)
 
 static inline void* getstackstart(coroutine_t* co)
 {
-    #ifdef USE_UCONTEXT
     // !! FIXME !!
     return co->stack + co->stacksize / 2;
-    #endif
     
     return (stack_direction() == 1)
         ? co->stack + STACKMARGIN
@@ -119,6 +143,7 @@ static inline void* getstackstart(coroutine_t* co)
 // gets coroutine from handle or NULL if does not exist
 static coroutine_t* getco(pdco_handle_t id)
 {
+    if (id <= 0) return NULL;
     if (id == PDCO_MAIN_ID) return &co_main;
     
     coroutine_t* co = pdco_first;
@@ -130,9 +155,63 @@ static coroutine_t* getco(pdco_handle_t id)
     return co;
 }
 
+#ifndef NDEBUG
+static int canaryok(coroutine_t* co)
+{
+    if (co == &co_main) return 0;
+    
+    if (!co) return 1;
+    if (*(uint32_t*)(co->stack) != CANARYS1) return 4;
+    if (*(uint32_t*)(co->stack + co->stacksize - sizeof(uint32_t)) != CANARYS2) return 5;
+    if (co->canary1 != CANARYC1) return 2;
+    if (co->canary2 != CANARYC2) return 3;
+    
+    return 0;
+}
+
+static void canarycheck(coroutine_t* co)
+{
+    #if !defined(TARGET_PLAYDATE) || defined(NEWLIB_COMPLETE)
+    switch (canaryok(co))
+    {
+    case 0:
+        canary_context = "";
+        return;
+    case 1:
+        printf("PDCO ERROR: coroutine is null (id %d) %s\n", co->id, canary_context);
+        break;
+    case 2:
+        printf("PDCO ERROR: canary C1 died (id %d) %s\n", co->id, canary_context);
+        break;
+    case 3:
+        printf("PDCO ERROR: canary C2 died (id %d) %s\n", co->id, canary_context);
+        break;
+    case 4:
+        printf("PDCO ERROR: canary S1 died (id %d) %s\n", co->id, canary_context);
+        break;
+    case 5:
+        printf("PDCO ERROR: canary S2 died (id %d) %s\n", co->id, canary_context);
+        break;
+    default:
+        printf("PDCO ERROR: unknown canary error (id %d) %s\n", co->id, canary_context);
+        break;
+    }
+    
+    assert(0);
+    #endif
+}
+#endif
+
 __attribute__((noinline))
 static void pdco_guard(void)
 {
+    #ifndef NDEBUG
+    canary_context = "guard (check prev)";
+    canarycheck(pdco_prev);
+    canary_context = "guard (check next)";
+    canarycheck(pdco_active);
+    #endif
+    
     cleanup_if_complete(pdco_prev);
     
     register pdco_handle_t resume_next;
@@ -140,9 +219,14 @@ static void pdco_guard(void)
     resume_next = pdco_active->fn(pdco_active->creator);
     asm("");
     
+    #ifndef NDEBUG
+    canary_context = "guard (end)";
+    canarycheck(pdco_active);
+    #endif
+    
     // final yield.
     pdco_active->status = 0; // mark thread as dirty / clean up
-    pdco_yield(resume_next); // after resuming, this thread will clean us up.
+    pdco_yield(resume_next); // after resuming, the next thread will clean us up.
 }
 
 // returns -1 if failure.
@@ -172,21 +256,33 @@ static int pdco_resume_(coroutine_t* nc)
 void pdco_yield(pdco_handle_t h)
 {
     coroutine_t* nc = getco(h);
+    #ifndef NDEBUG
+    canary_context = "yield";
+    canarycheck(nc);
+    #endif
     if (pdco_resume_(nc) != 0)
     {
-        // crash
+        #if !defined(TARGET_PLAYDATE) || defined(NEWLIB_COMPLETE)
+        assert(0);
+        #endif
+        
+        // causes crash
         pdco_resume_(nc);
     }
 }
 
 pdco_handle_t pdco_create(pdco_fn_t fn, size_t stacksize, void* ud)
 {
+    init();
+    
     #if defined(__arm__) || defined(__clang__)
     register
     #endif
     coroutine_t* nc = (coroutine_t*)malloc(sizeof(coroutine_t));
     if (!nc) return -1;
     memset(nc, 0, sizeof(coroutine_t));
+    nc->canary1 = CANARYC1;
+    nc->canary2 = CANARYC2;
     
     #ifdef USE_UCONTEXT
         if (getcontext(&pdco_active->ucalt) < 0)
@@ -198,18 +294,29 @@ pdco_handle_t pdco_create(pdco_fn_t fn, size_t stacksize, void* ud)
     
     nc->stack = NULL;
     if (stacksize == 0) stacksize = 1 << 10;
+    nc->stacksize = stacksize;
     nc->stack = malloc(stacksize);
     if (!nc->stack)
     {
         free(nc);
         return -1;
     }
+    *(uint32_t*)(nc->stack) = CANARYS1;
+    *(uint32_t*)(nc->stack + stacksize - sizeof(uint32_t)) = CANARYS2;
+    #ifndef NDEBUG
+    canary_context = "create (start)";
+    canarycheck(nc);
+    #endif
     nc->ud = ud;
     nc->stackstart = getstackstart(nc);
-    nc->stacksize = stacksize;
     nc->status = 1; // running
     nc->fn = fn;
     nc->creator = pdco_active->id;
+    
+    #ifndef NDEBUG
+    canary_context = "create (middle)";
+    canarycheck(nc);
+    #endif
     
     // assign unique ID
     nc->id = next_coroutine_id++;
@@ -245,16 +352,23 @@ pdco_handle_t pdco_create(pdco_fn_t fn, size_t stacksize, void* ud)
         }
     #endif
     
+    #ifndef NDEBUG
+    canary_context = "create (end)";
+    canarycheck(nc);
+    #endif
+    
     return nc->id;
 }
 
 pdco_handle_t pdco_current(void)
 {
+    init();
     return pdco_active->id;
 }
 
 void** pdco_ud(pdco_handle_t h)
 {
+    init();
     coroutine_t* c = pdco_active;
     if (h > 0) c = getco(h);
     return &c->ud;
@@ -262,6 +376,7 @@ void** pdco_ud(pdco_handle_t h)
 
 int pdco_exists(pdco_handle_t h)
 {
+    init();
     coroutine_t* c = getco(h);
     if (c != NULL)
     {
